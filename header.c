a888fcfeb42fdd50263f6b7ddeb72c0df96dd12a
Valeriu Ohan <vo2@sanger.ac.uk>
2019-01-15 09:48:40 +0000

    Add method `bam_hdr_nref` for returning the number of references inside the header. Minor fixes.

--- header.c
+++ header.c
@@ -801,17 +801,24 @@ static void redact_header_text(bam_hdr_t *bh) {
 /* ==== Public methods ==== */
 
 int bam_hdr_length(bam_hdr_t *bh) {
-    if (-1 == bam_hdr_rebuild(bh))
+    if (!bh || -1 == bam_hdr_rebuild(bh))
         return -1;
 
     return bh->l_text;
 }
 
 const char *bam_hdr_str(bam_hdr_t *bh) {
-    if (-1 == bam_hdr_rebuild(bh))
+    if (!bh || -1 == bam_hdr_rebuild(bh))
         return NULL;
 
     return bh->text;
+}
+
+int bam_hdr_nref(const bam_hdr_t *bh) {
+    if (!bh || !bh->hdr)
+        return -1;
+
+    return bh->hdr->nref;
 }
 
 /*
@@ -1211,7 +1218,7 @@ int bam_hdr_remove_lines(bam_hdr_t *bh, const char *type, const char *id, void *
 
 /* ==== Key:val level methods ==== */
 
-const char *bam_hdr_find_tag(bam_hdr_t *bh,
+char *bam_hdr_find_tag(bam_hdr_t *bh,
         const char *type,
         const char *ID_key,
         const char *ID_value,
